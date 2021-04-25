95be6e678f482ebcdd1657ad8394e7a7e3810679
John Cambefort <jcambefort@middlebury.edu>
2021-04-25 09:46:06 -0400

    More commenting

--- preprocessing/fetch_data.py
+++ preprocessing/fetch_data.py
@@ -5,8 +5,8 @@ import requests
 import json
 import geopandas as gpd
 
-# Downloads data as JSON files from the Vermont Geoportal REST API
 def request_API_data(url, path):
+# Downloads data as JSON files from the Vermont Geoportal REST API
     
     r = requests.get(url) # Make the request for trail data from VT Geoportal
     if not(r.raise_for_status()):
@@ -27,6 +27,7 @@ def json_to_geojson(json_file_path, out_path):
 
 
 def fetch_green_mt_trails():
+    # Returns a GeoJson file of trails
     green_mountain_trails_url = "https://opendata.arcgis.com/datasets/d64e921c74db49f589e14b24d9bd779a_12.geojson"
     trails_json_path = "trails.json"
     trails_geojson_path = "trails.geojson"
@@ -37,6 +38,7 @@ def fetch_green_mt_trails():
     return trails_geojson_path
 
 def fetch_e911_trails():
+    # Returns a GeoJson file of trails from the Vermont E911 dataset
     e911_trails_url = "https://opendata.arcgis.com/datasets/df69f82eb91e492d89223ae50d2f89b5_21.geojson"
     e911_json_path = "e911_trails.json"
     e911_geojson_path = "e911_trails.geojson"
@@ -47,6 +49,8 @@ def fetch_e911_trails():
     return 
 
 def fetch_counties():
+    # Returns a GeoJson file of Vermont counties
+    # This will be used to extract the Addison County polygon
     counties_url = "https://opendata.arcgis.com/datasets/2f289dbae90347c58cd1765db84bd09e_29.geojson"
     counties_json_path = "counties.json"
     counties_geojson_path = "counties.geojson"
@@ -66,6 +70,7 @@ def filter_addison_poly(counties_path, out_path):
 
 
 if __name__ == "__main__":
-    fetch_green_mt_trails()
+    fetch_green_mt_trails() # Download VT Green Mountain trail data
+    fetch_e911_trails()     # Download E911 Vermont trail data
     counties_path = fetch_counties()
-    filter_addison_poly(counties_path, "addison.geojson")
+    filter_addison_poly(counties_path, "addison.geojson") # Fetch Addison county
